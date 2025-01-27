6d181d9a721edd91d5f45d9a73f94f0c35ea7edc
MisoSoup-W <wavesmile17@gmail.com>
2025-01-27 20:27:04 +0700

    add new file

---
+++ 02_git.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    printf("This is my second git");
+    int i = 10;
+    printf("Change git : %d", i * 2);
+    printf("Try to add and update git");
+return 0;
+}
